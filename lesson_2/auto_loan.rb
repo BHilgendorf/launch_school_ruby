62199e4dc247eea143af3551c1152bf47ded3c97
Betsy Hilgendorf <betsy.m.hilgendorf@gmail.com>
2016-05-01 09:52:17 -0600

    changes made may 1

--- lesson_2/auto_loan.rb
+++ lesson_2/auto_loan.rb
@@ -1,11 +1,17 @@
 # calculates the montly payment on an auto loan with user inputs of loan amount, APR and loan duration.
 
+def clear_screen
+  system('clear') || system('cls')
+end
+
 def integer?(num)
   num.to_i.to_s == num && num.to_i > 0
 end
 
-def float?(num)
-  num.to_f.to_s == num && num.to_f > 0
+def float?(string)
+  Float(string)
+rescue ArgumentError
+  nil
 end
 
 puts "Welcome to the Auto Loan Calculator"
@@ -18,17 +24,17 @@ loop do # main loop
   loop do
     loan_amount = gets.chomp
 
-    if integer?(loan_amount)
-      loan_amount = loan_amount.to_i
+    if float?(loan_amount) && loan_amount.to_f > 0
+      loan_amount = loan_amount.to_f
       break
     else
-      puts "That is not a valid amount. Please enter an amount, no need for $ or commas."
+      puts "Please enter a positive amount, no need for $ or commas or periods."
     end
   end
 
   apr = " "
-  puts "Enter Anual Percentage Rate. (Ex: For 4% enter 4.0  For 3.5% enter 3.5)"
-  
+  puts "Enter Annual Percentage Rate. (Ex: For 4% enter 4.0  For 3.5% enter 3.5)"
+
   loop do
     apr = gets.chomp
 
@@ -36,7 +42,7 @@ loop do # main loop
       apr = apr.to_f
       break
     else
-      puts "That is not a valid percentage rate. Please enter the rate above 0.0 "
+      puts "That is not a valid percentage rate."
     end
   end
 
@@ -54,15 +60,26 @@ loop do # main loop
     end
   end
 
-  monthly_rate = apr / 12 / 100
-  monthly_payment = (loan_amount * ((monthly_rate * (1 + monthly_rate)**months) /
-      ((1 + monthly_rate)**months - 1))).round(2)
+  if apr == 0
+    monthly_payment = loan_amount / months
+  else
+    monthly_rate = apr / 12 / 100
+    monthly_payment = (loan_amount * ((monthly_rate * (1 + monthly_rate)**months) /
+        ((1 + monthly_rate)**months - 1)))
+  end
 
-  puts "The monthly payment will be:$#{monthly_payment}"
-  puts "Do you want to perform another calculation? (Y to calculate again)"
-  answer = gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  puts "Your monthly payment is: $#{format('%02.2f', monthly_payment)}"
+  puts "Do you want to perform another calculation? (Y or N)"
+
+  answer = ""
+  loop do
+    answer = gets.chomp.upcase
+    break if answer == 'Y' || answer == 'N'
+    puts "Y or N only please"
+  end
+  break unless answer == 'Y'
+
+  clear_screen
 end
 
-puts "Enjoy your new car!"
 puts "Goodbye!"
