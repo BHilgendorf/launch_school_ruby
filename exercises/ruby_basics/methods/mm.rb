c5e45679d2755f4ea5498ed18493829d5cbe33fb
Betsy Hilgendorf <betsy.m.hilgendorf@gmail.com>
2016-07-14 19:13:28 -0600

    all method exercises

---
+++ exercises/ruby_basics/methods/mm.rb
@@ -0,0 +1,15 @@
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+car("Toyota", "Tacoma")
+car("Jeep", "Cherokee")   #outputs "Jeep Cherokee" but returns nil
+
+
+# #futher exploration
+
+# def car(make, model)
+#   make + " " + model
+# end                          
+
+# p car("Subaru", "Brat")   # outputs "Subaru Brat and returns a string of "Subaru Brat"
