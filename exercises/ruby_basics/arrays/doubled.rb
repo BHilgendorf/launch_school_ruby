ab2189d38ebf8ffe9dd1b2abf57945286197ddd7
Betsy Hilgendorf <betsy.m.hilgendorf@gmail.com>
2016-07-16 15:09:17 -0600

    array practice

---
+++ exercises/ruby_basics/arrays/doubled.rb
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+
+doubled_numbers = numbers.map {|digit| digit * 2}
+
+p doubled_numbers
+
---
+++ exercises/ruby_basics/arrays/pets.rb
@@ -0,0 +1,10 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pet = pets[2]
+
+puts "I have a pet #{my_pet}!"
+
+#If the index is unknown, but the value is known, select can be used
+# my_pet = pets.select {|type| type == 'fish'}
+
+# puts "I have a pet #{my_pet}"
